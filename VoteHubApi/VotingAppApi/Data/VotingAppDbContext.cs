8eeb9afda5fed3ca94f01edc24bac4b19a0a5925
Chukky-27 <cartusky4eva88@gmail.com>
2024-12-05 14:10:56 +0100

    Models

---
+++ VoteHubApi/VotingAppApi/Data/VotingAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VotingAppApi.Models;
+
+namespace VotingAppApi.Data
+{
+    public class VotingAppDbContext : DbContext 
+    {
+        public VotingAppDbContext(DbContextOptions<VotingAppDbContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+
+    
+}
---
+++ VoteHubApi/VotingAppApi/Models/Candidate.cs
@@ -0,0 +1,10 @@
+﻿namespace VotingAppApi.Models
+{
+    public record Candidate
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Position { get; set; }
+        public int VoteCount { get; set; }
+    }
+}
